a4fe7fdf6cbb3f6e728c6102c1e56ccfc448a353
H-goto16 <h.goto.engineer@gmail.com>
2023-12-26 20:45:25 +0900

    fix: divide function

--- image2text/__init__.py
+++ image2text/__init__.py
@@ -1,1 +1,2 @@
-from image2text.main import *
+from image2text.realtime_i2t import *
+from image2text.i2t_from_frame import *
--- image2text/i2t_from_frame.py
+++ image2text/i2t_from_frame.py
@@ -1,29 +1,23 @@
-import cv2
 from transformers import BlipProcessor, BlipForConditionalGeneration
 import torch
 
-def get_device():
-    return "cuda" if torch.cuda.is_available() else "cpu"
-
-def i2t(camera_id=0, cache_dir="cache"):
+def i2t_from_frame(frame, cache_dir="cache"):
     """
     + image2text
     ```python
-    from image2text import i2t
+    from image2text import i2t_from_frame
 
-    for text in i2t():
+    camera = cv2.VideoCapture(0)
+    while True:
+        ret, frame = camera.read()
+        if not ret:
+            break
+        text = i2t_from_frame(frame)
         print(text)
     ```
-
-    args:
-        camera_id: camera id, default: 0
-        cache_dir: cache directory, default: "cache"
-    yield:
-        text: text of image
     """
-    cap = cv2.VideoCapture(camera_id)
     processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large", cache_dir=cache_dir)
-    device = get_device()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
     print("Using device: ", device.upper())
     if (device == "cuda"):
@@ -32,16 +26,9 @@ def i2t(camera_id=0, cache_dir="cache"):
         model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large", cache_dir=cache_dir)
 
     while True:
-        ret, frame = cap.read()
-        if not ret:
-            pass
         if device == "cuda":
             processor=processor(frame, "a photography of", return_tensors="pt").to(device, torch.float16)
         else:
             inputs = processor(frame, "a photography of", return_tensors="pt")
         out = model.generate(**inputs)
-        yield processor.decode(out[0], skip_special_tokens=True)
-        cv2.imshow("image2text", frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-    cap.release()
+        return processor.decode(out[0], skip_special_tokens=True)
